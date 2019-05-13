12c3831279ee37126d364c0aab9c59e5277aecaa
Mario Alvial <mse.alvial@gmail.com>
2019-05-13 20:52:10 -0300

    Add javadoc of health info

--- src/main/kotlin/io/github/marioalvial/kealth/HealthInfo.kt
+++ src/main/kotlin/io/github/marioalvial/kealth/HealthInfo.kt
@@ -2,6 +2,8 @@ package io.github.marioalvial.kealth
 
 /**
  * Class that aggregates information about the execution of component's health check
+ * @property status Status of health component
+ * @property duration Duration of component's health check execution
  */
 class HealthInfo(
     val status: HealthStatus,
